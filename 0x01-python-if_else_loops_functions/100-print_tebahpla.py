5a17c506c7d40ae1c513a45ba2594aea219aa19b
JosephK89 <josephkamel262@gmail.com>
2021-04-27 21:48:11 +0300

    Add python project

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for x in range(122, 95, -1):
-    print("{:c}".format((x - 32)) if c % 2 else c), end="")
+for x in range(90, 66):
+    if (x % 2 == 0):
+    	print("{:c}".format(x + 32), end="")
+    else:
+	print("{:c}".format(x), end="")
