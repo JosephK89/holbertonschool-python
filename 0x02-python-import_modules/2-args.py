8c01affa7b546d9e46817bef5654b256706ee5b3
JosephK89 <josephkamel262@gmail.com>
2021-04-29 15:25:10 +0300

    Add python project

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    x = len(argv) - 1
-    print("{:d} {:s}{:s}".format(x, "argument" if x == 1 else "arguments","." if x == 0 else ":"))
+    x = len(argv)
+    print("{:d} {:s}{:s}".format(x - 1, "argument" if x == 1 else "arguments","." if x == 0 else ":"))
     for i in range(x):
         if i > 0:
-            print("{:d}: {:s}".format(i, arvg(i + 1))
+            print("{:d}: {:s}".format(i, arvg[i])
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    x = len(argv) - 1
+    x = len(argv)
     sum = 0
     for i in range (x):
-        sum += int(arvg(i + 1))
+        sum += int(arvg[i + 1])
     print("{:d}".format(sum))
