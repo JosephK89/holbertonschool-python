2c20f3fd0ffb027025a66f95935a9d3f8f9ffd33
JosephK89 <josephkamel262@gmail.com>
2021-04-29 15:14:17 +0300

    Add python project

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    x = len(argv)
+    print("{:d} {:s}{:s}".format(x - 1, "argument" if x == 1 else "arguments","." if x == 0 else ":"))
+    for i in range(x):
+        if i > 0:
+            print("{:d}: {:s}".format(i, arvg(i + 1))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    x = len(argv)
+    sum = 0
+    for i in range (x):
+        sum += int(arvg(i + 1))
+    print("{:d}".format(sum))
