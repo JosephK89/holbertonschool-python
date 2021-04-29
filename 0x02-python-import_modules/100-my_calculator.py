c7a85dc83bfa3f2d5131bf203f477377b3c2e5d5
JosephK89 <josephkamel262@gmail.com>
2021-04-29 17:47:34 +0300

    Add python project

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,14 +2,13 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    
-    if len(sys.argv) != 4:
+    args = len(sys.argv)
+    if args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
-    
     if op == "+":
         print("{} {} {} = {}".format(a, op, b, add(a, b)))
     elif op == "-":
