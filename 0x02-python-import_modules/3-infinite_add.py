3b53e6d244ebdd8ebaa947259091ca6cc8c9e5a6
JosephK89 <josephkamel262@gmail.com>
2021-04-29 15:56:14 +0300

    Add python project

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     x = len(sys.argv)
     sum = 0
     for i in range(1, x):
-        sum += int(sys.arvg[i])
+        sum += int(sys.argv[i])
     print("{}".format(sum))
