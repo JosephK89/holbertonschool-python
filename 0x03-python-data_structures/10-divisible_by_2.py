5db8ef7a454df2d31d802543e4405edfce5b278a
JosephK89 <josephkamel262@gmail.com>
2021-05-03 14:28:03 +0300

    Add python project

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     nlist = []
     for i in my_list:
-        if i%2 == 0:
+        if i % 2 == 0:
             nlist.append(True)
         else:
             nlist.append(False)
