b3e632612d38efafd246d5cee0e05d948a3ca6c7
JosephK89 <josephkamel262@gmail.com>
2021-05-03 14:53:42 +0300

    Add python project

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string=''.join(i for i in my_string if not i in "cC")
+    my_string="".join(i for i in my_string if i not in "cC")
     return my_string
