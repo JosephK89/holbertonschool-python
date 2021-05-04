9a9bde7573e9ae0ca6aa71ca3f6948738bd5bd5c
JosephK89 <josephkamel262@gmail.com>
2021-05-04 20:18:26 +0300

    Add python project

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     values = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     if type(roman_string) is not str or roman_string is None:
         return 0
-    sum=0
+    sum = 0
     while i < len(roman_string) - 1
         a = values[roman_string[i]]
         b = values[roman_string[i + 1]]
