d9443438f46bce4bedda089d969fdb1a1a10f327
JosephK89 <josephkamel262@gmail.com>
2021-05-04 21:15:25 +0300

    Add python project

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list and len(my_list):
+    if my_list is None:
+        return 0
+    else:
         num = 0
         deno = 0
         for i in my_list:
             num += i[0]*i[1]
             dem += i[1]
         return (num / dem)
-    return 0
