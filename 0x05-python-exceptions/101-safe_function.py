538ea1567c93c7dc111a6a35048f8fd7838bbd87
JosephK89 <josephkamel262@gmail.com>
2021-05-06 15:53:57 +0300

    Add python project

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        f = fct(*args)
+        return f
+    except Exception as ex:
+        print("Exception:", ex, file=sys.stderr)
+        return None
