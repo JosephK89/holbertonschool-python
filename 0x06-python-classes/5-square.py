43dd8906bcff4d26d7f57753418e1eed82221fad
JosephK89 <josephkamel262@gmail.com>
2021-05-10 22:12:06 +0300

    Add python project

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,32 +3,44 @@
 
 
 class Square:
-    """squareclass"""
+    """square class
+    """
     def __init__(self, size=0):
+        """initializes the square
+        """
         self.size = size
 
     def area(self):
-        """area function"""
-        return (self.__size)**2
-    
+        """area fct
+        """
+        return (self.__size) ** 2
+
     @property
     def size(self):
-        """"getter fct"""
+        """getter fct
+        """
         return self.__size
-        
+
     @size.setter
     def size(self, value):
-        """setter fct"""
+        """setter fct
+        """
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
     
     def my_print(self):
-        """print square"""
+        """prints the square
+
+        Returns:
+            None
+        """
         if self.__size == 0:
             print()
             return
         for i in range(self.__size):
-            print("#" * self.__size)
+            print("".join(["#" for j in range(self.__size)]))
