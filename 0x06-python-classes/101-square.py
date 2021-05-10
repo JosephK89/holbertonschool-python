ee7d87707614f30a960f007be3ab2528aa9923cf
JosephK89 <josephkamel262@gmail.com>
2021-05-10 22:20:14 +0300

    Add python project

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -6,7 +6,7 @@ class Square:
     """square class
     """
     def __init__(self, size=0, position=(0, 0)):
-        """initialization of the class
+        """initializes the square
         """
         self.size = size
         self.position = position
@@ -54,9 +54,11 @@ class Square:
 
     @position.setter
     def position(self, value):
-        """setter fct
+        """setterfct
         """
-        if type(value) is not tuple or len(value) != 2 or type(value[0]) is not int or value[0] < 0 or type(value[1]) is not int or value[1] < 0:
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or value[0] < 0 or \
+           type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,46 +3,62 @@
 
 
 class Square:
-    """squareclass"""
+    """square class
+    """
     def __init__(self, size=0, position=(0, 0)):
+        """initializes the square
+        """
         self.size = size
         self.position = position
 
     def area(self):
-        """area function"""
-        return (self.__size)**2
-    
+        """area fct
+        """
+        return (self.__size) ** 2
+
     @property
     def size(self):
-        """"getter fct"""
+        """getterfct
+        """
         return self.__size
-        
+
     @size.setter
     def size(self, value):
-        """setter fct"""
+        """setterfct
+        """
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-    
-    @property
-    def position(self):
-        """"getter fct"""
-        return self.__position
-        
-    @position.setter
-    def position(self, value):
-        """setter fct"""
-        if type(value) is not tuple or len(value) != 2 or type(value[0]) is not int or value[0] < 0 or type(value[1]) is not int or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            self.__position = value
-        
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
     def my_print(self):
-        """print square"""
+        """print fct
+        """
         if self.__size == 0:
             print()
             return
-        for i in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
+        for i in range(self.__position[1]):
+            print()
+        for j in range(self.__size):
+            print("".join([" " for k in range(self.__position[0])]), end="")
+            print("".join(["#" for l in range(self.__size)]))
+
+    @property
+    def position(self):
+        """getter fct
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """setterfct
+        """
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or value[0] < 0 or \
+           type(value[1]) is not int or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
