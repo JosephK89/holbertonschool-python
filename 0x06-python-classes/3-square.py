f5e7f0abd30843b4a5a61c90ca54384cf2d5fcc1
JosephK89 <josephkamel262@gmail.com>
2021-05-10 19:18:27 +0300

    Add python project

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,6 +11,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     
-    """area function"""
     def area(self):
+        """area function"""
         return (self.__size) ** 2
