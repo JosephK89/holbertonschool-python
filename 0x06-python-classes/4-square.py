604bd4451203806a0fe643329b357af3e9be5ce3
JosephK89 <josephkamel262@gmail.com>
2021-05-10 19:51:30 +0300

    Add python project

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""define a class"""
+
+
+class Square:
+    """squareclass"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        """area function"""
+        return (self.__size)**2
+    
+    @property
+    def size(self):
+        """"getter fct"""
+        return self.__size
+        
+    @size.setter
+    def size(self, value):
+        """setter fct"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
