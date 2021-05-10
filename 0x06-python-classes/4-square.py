bb63be49d8b62866c09a5326c22d95a315af6584
JosephK89 <josephkamel262@gmail.com>
2021-05-10 19:53:49 +0300

    Add python project

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -19,8 +19,8 @@ class Square:
     @size.setter
     def size(self, value):
         """setter fct"""
-        if type(size) is not int:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
