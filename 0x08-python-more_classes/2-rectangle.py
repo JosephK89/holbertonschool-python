8b7ebed6ca0c5a48b035bf4ef932f857f7376c9e
JosephK89 <josephkamel262@gmail.com>
2021-05-17 21:06:15 +0300

    Add python project

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -7,7 +7,7 @@ rectangle module
 class Rectangle:
     """rectangle class"""
     def __init__(self, width=0, height=0):
-        """rectangle"""
+        """Initializes the rectangle"""
         self.width = width
         self.height = height
 
@@ -38,13 +38,13 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-    
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+
     def perimeter(self):
         """returns the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.__width + self.__height) * 2
-    
-    def area(self):
-        """returns the area of the rectangle"""
-        return self.__width * self.__height
+        return (self.__width * 2) + (self.__height * 2)
