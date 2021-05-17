a1f61cac09d35af2d16bf134ab1a11f9b1666126
JosephK89 <josephkamel262@gmail.com>
2021-05-17 20:47:09 +0300

    Add python project

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,14 +8,14 @@ class Rectangle:
     """rectangle class"""
     def __init__(self, width=0, height=0):
         """rectangle"""
-        self.width = width
         self.height = height
-        
+        self.width = width
+
     @property
     def width(self):
         """getter"""
         return self.__width
-        
+
     @width.setter
     def width(self, value):
         """setter"""
@@ -24,12 +24,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
+
     @property
     def height(self):
         """getter"""
         return self.__height
-        
+
     @height.setter
     def height(self, value):
         """setter"""
