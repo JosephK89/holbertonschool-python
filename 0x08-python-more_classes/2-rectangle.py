fc520ae48a586108401b3c378ff3c396e0fa9355
JosephK89 <josephkamel262@gmail.com>
2021-05-17 20:56:04 +0300

    Add python project

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -8,14 +8,14 @@ class Rectangle:
     """rectangle class"""
     def __init__(self, width=0, height=0):
         """rectangle"""
-        self.height = height
         self.width = width
-        
+        self.height = height
+
     @property
     def width(self):
         """getter"""
         return self.__width
-        
+
     @width.setter
     def width(self, value):
         """setter"""
@@ -24,12 +24,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
+
     @property
     def height(self):
         """getter"""
         return self.__height
-        
+
     @height.setter
     def height(self, value):
         """setter"""
--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -8,14 +8,14 @@ class Rectangle:
     """rectangle class"""
     def __init__(self, width=0, height=0):
         """rectangle"""
-        self.height = height
         self.width = width
-        
+        self.height = height
+
     @property
     def width(self):
         """getter"""
         return self.__width
-        
+
     @width.setter
     def width(self, value):
         """setter"""
@@ -24,12 +24,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
+
     @property
     def height(self):
         """getter"""
         return self.__height
-        
+
     @height.setter
     def height(self, value):
         """setter"""
@@ -48,7 +48,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
-        
+    
     def __str__(self):
         """returns the rectangle"""
         string = ""
