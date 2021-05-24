d226bf9b5055ee8fb6b437fa551a8acd8a7a7556
JosephK89 <josephkamel262@gmail.com>
2021-05-24 22:04:17 +0300

    Add python project

--- 0x0B-python-inheritance/1-my_list.py
+++ 0x0B-python-inheritance/1-my_list.py
@@ -4,8 +4,8 @@ mylist module
 """
 
 
-class Mylist(list):
-    """mylist class"""
+class MyList(list):
+    """mylistclass"""
     def print_sorted(self):
         """prints the list in ascending order"""
         print(sorted(self))
