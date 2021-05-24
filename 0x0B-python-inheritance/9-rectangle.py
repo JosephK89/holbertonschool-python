9398078069675e0d54cd71b91e46c1443b531150
JosephK89 <josephkamel262@gmail.com>
2021-05-24 20:50:27 +0300

    Add python project

--- 0x0B-python-inheritance/9-rectangle.py
+++ 0x0B-python-inheritance/9-rectangle.py
@@ -2,9 +2,9 @@
 """
 rectangle module
 """
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
 class Rectangle(BaseGeometry):
     """rectangle class"""
     def __init__(self, width, height):
