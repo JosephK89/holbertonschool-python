5fac4499435ed273ddf484eb1a5634039fea2a8d
JosephK89 <josephkamel262@gmail.com>
2021-05-24 19:04:41 +0300

    Add python project

--- 0x0B-python-inheritance/100-my_int.py
+++ 0x0B-python-inheritance/100-my_int.py
@@ -7,7 +7,9 @@ myint module
 class MyInt(int):
     """MyInt class"""
     def __eq__(self, other):
+        """eq"""
         return super().__ne__(other)
     
     def __ne__(self, other):
+        """ne"""
         return super().__eq__(other)
