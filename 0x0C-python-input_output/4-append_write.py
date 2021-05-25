28f5e6956843a59a6d70f947a3864161b266553d
JosephK89 <josephkamel262@gmail.com>
2021-05-25 11:41:45 +0300

    Add python project

--- 0x0C-python-input_output/4-append_write.py
+++ 0x0C-python-input_output/4-append_write.py
@@ -6,6 +6,6 @@ append write module
 
 def append_write(filename="", text=""):
     """append_write fct"""
-    with open(filename, mode = "a", encoding="utf-8") as file:
+    with open(filename, mode="a", encoding="utf-8") as file:
         characters = file.write(text)
     return characters
