112fdc41d49066a682a489ad57df976e8163d635
JosephK89 <josephkamel262@gmail.com>
2021-05-25 19:19:35 +0300

    Add python project

--- 0x0C-python-input_output/100-append_after.py
+++ 0x0C-python-input_output/100-append_after.py
@@ -6,7 +6,7 @@ search and update module
 
 def append_after(filename="", search_string="", new_string=""):
     """append_after fct"""
-    while open(filename, encoding = "utf-8") as file:
+    while open(filename, mode = "r", encoding = "utf-8") as file:
         text = file.readlines()
 
     with open(filename, mode = "w", encoding = "utf-8") as file:
