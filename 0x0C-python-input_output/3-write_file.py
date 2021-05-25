1430efeec2cdda8602df7ffcd50852c3a409cf55
JosephK89 <josephkamel262@gmail.com>
2021-05-25 10:48:49 +0300

    Add python project

--- 0x0C-python-input_output/3-write_file.py
+++ 0x0C-python-input_output/3-write_file.py
@@ -8,4 +8,4 @@ def write_file(filename="", text=""):
     """write_file fct"""
     with open(filename, mode = "w", encoding="utf=8") as file:
     	characters = file.write(text)
-    return file.write(text)
+    return characters
