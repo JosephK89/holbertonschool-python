c1f93bdf861189681c7f4d3877dc1bd0e21a20c0
JosephK89 <josephkamel262@gmail.com>
2021-05-25 09:52:20 +0300

    Add python project

--- 0x0C-python-input_output/0-read_file.py
+++ 0x0C-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ read file module
 def read_file(filename=""):
     """read_file fct"""
     with open(filename, encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
