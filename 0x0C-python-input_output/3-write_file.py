f6e678ab67cef4e07bed3ea28f43cf0f9baa8155
JosephK89 <josephkamel262@gmail.com>
2021-05-25 10:45:12 +0300

    Add python project

--- 0x0C-python-input_output/3-write_file.py
+++ 0x0C-python-input_output/3-write_file.py
@@ -5,6 +5,6 @@ write to a file module
 
 
 def write_file(filename="", text=""):
-    """write_file fct"""
-    with open(filename, mode = "w", encoding="utf-8") as file:
+    """write_file fct" """
+    with open(filename, mode = "w", encoding='utf=8') as file:
         return file.write(text)
