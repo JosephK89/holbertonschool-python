de744846c48be63e13685cd6d9e0ffb189071068
JosephK89 <josephkamel262@gmail.com>
2021-05-25 15:38:38 +0300

    Add python project

--- 0x0C-python-input_output/11-student.py
+++ 0x0C-python-input_output/11-student.py
@@ -11,7 +11,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self):
-        """to_json fct"""
+        """to_json funct"""
         return self.__dict__
--- 0x0C-python-input_output/9-add_item.py
+++ 0x0C-python-input_output/9-add_item.py
@@ -2,11 +2,15 @@
 """
 json module
 """
+import sys
+import os.path
 
 save = __import__('7-save_to_json_file.py').save_to_json_file
 load = __import__('8-load_from_json_file.py').load_from_json_file
 
-
 filename = "add_item.json"
-json_list = load_from_json_file(filename)
-save_to_json_file(json_list, filename)
+list = []
+if os.path.exists(filename) and os.path.getsize(my_file) > 0:
+    list = load_from_json_file(filename)
+
+save_to_json_file(list + sys,argv[1:], filename)
