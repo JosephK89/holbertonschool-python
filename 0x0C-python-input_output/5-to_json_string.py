838634c0558c3c52af9c19eb8e981cbbb55e6b85
JosephK89 <josephkamel262@gmail.com>
2021-05-25 11:15:32 +0300

    Add python project

--- 0x0C-python-input_output/5-to_json_string.py
+++ 0x0C-python-input_output/5-to_json_string.py
@@ -4,6 +4,7 @@ to json string module
 """
 import json
 
+
 def to_json_string(my_obj):
     """to_json_string fct"""
     return json.dumps(my_obj)
--- 0x0C-python-input_output/6-from_json_string.py
+++ 0x0C-python-input_output/6-from_json_string.py
@@ -4,6 +4,7 @@ from json string module
 """
 import json
 
+
 def from_json_string(my_str):
     """from_json_string fct"""
     return json.load(my_str)
