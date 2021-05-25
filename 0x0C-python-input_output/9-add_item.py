e275de424219197b9d1dc275051993b7209ffd22
JosephK89 <josephkamel262@gmail.com>
2021-05-25 15:57:03 +0300

    Add python project

--- 0x0C-python-input_output/9-add_item.py
+++ 0x0C-python-input_output/9-add_item.py
@@ -2,19 +2,22 @@
 """
 json module
 """
+
 import sys
+import os.path
 
-save = __import__("7-save_to_json_file").save_to_json_file
-load = __import__("8-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
-filename = "add_item.json"
+my_file = 'add_item.json'
 
-try:
-    json_list = load(filename)
-except:
-    json_list = []
+my_list = []
 
-for arg in argv[1:]:
-    json_list.append(arg)
+if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
+    my_list = load_from_json_file(my_file)
 
-save(json_list, filename)
+if len(sys.argv) > 1:
+    for elem in sys.argv[1:]:
+        my_list.append(elem)
+
+save_to_json_file(my_list, my_file)
