4d7d32fea1aa459fe2b3d95dbb702e0c0f5483fe
JosephK89 <josephkamel262@gmail.com>
2021-05-25 13:28:49 +0300

    Add python project

--- 0x0C-python-input_output/7-save_to_json_file.py
+++ 0x0C-python-input_output/7-save_to_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """save_to_json_file fct"""
-    with open(filename, mode = "w", encoding = "utf-8") as file:
+    with open(filename, mode = "w", encoding ="utf-8") as file:
         file.write(json.dump(my_object))
