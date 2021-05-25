6e2908ef729dc18c580c14c8e82cd04c944d8eeb
JosephK89 <josephkamel262@gmail.com>
2021-05-25 11:31:30 +0300

    Add python project

---
+++ 0x0C-python-input_output/3-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+write to a file module
+"""
+
+
+def write_file(filename="", text=""):
+    """write_file fct"""
+    with open(filename, mode = "w", encoding="utf=8") as file:
+        characters = file.write(text)
+    return characters
