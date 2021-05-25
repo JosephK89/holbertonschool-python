65b97459a2f8f52f75640be42d379c4aed77b0c9
JosephK89 <josephkamel262@gmail.com>
2021-05-25 14:24:06 +0300

    Add python project

--- 0x0C-python-input_output/3-write_file.py
+++ 0x0C-python-input_output/3-write_file.py
@@ -6,5 +6,6 @@ write to a file module
 
 def write_file(filename="", text=""):
     """write file fct"""
-    with open(filename, mode = "w", encoding = "utf-8") as file:
-        return file.write(text)
+    with open(filename, mode="w", encoding="utf-8") as file:
+        characters = file.write(text)
+    return characters
--- 0x0C-python-input_output/8-load_from_json_file.py
+++ 0x0C-python-input_output/8-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def load_from_json_file(filename):
     """load_from_json_file fct"""
-    with open(filename, mode="w", encoding="utf-8") as file:
+    with open(filename, mode="r", encoding="utf-8") as file:
         return json.load(file)
