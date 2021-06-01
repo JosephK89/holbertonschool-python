5c47634dda0771164d6541844c95e57a1d5fa40d
JosephK89 <josephkamel262@gmail.com>
2021-06-01 12:36:29 +0300

    Add python project

--- 0x0D-python-almost_a_circle/models/base.py
+++ 0x0D-python-almost_a_circle/models/base.py
@@ -6,13 +6,13 @@ base module
 
 class Base:
     """base class"""
-    
+
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
-        """initialization of the class"""
+        """Initialization of the class"""
         if id is not None:
             self.id = id
         else:
-            Base.__nb_object += 1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
