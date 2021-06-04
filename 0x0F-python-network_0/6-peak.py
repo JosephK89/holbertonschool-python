dee8bef150d829282e67fe92c729976674b778e9
JosephK89 <josephkamel262@gmail.com>
2021-06-04 22:09:57 +0300

    Add project

--- 0x0F-python-network_0/6-peak.py
+++ 0x0F-python-network_0/6-peak.py
@@ -5,21 +5,14 @@ find peak module
 
 
 def find_peak(list_of_integers):
-    """find_peak fct."""
-    if not list_of_integers:
-        return None
-    start = 0
-    l = len(list_of_integers)
-    end = len(list_of_integers) - 1
-    mid = (start + end) / 2
-    mid = int(mid)
-    if l == 1:
-        return list_of_integers[0]
-    if l == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid -1]:
-        return find_peak(list_of_integers[:mid])
+    """find_peak fct"""
+    li = list_of_integers
+    l = len(li)
+    if l == 0:
+        return
+    m = l // 2
+    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
+        return li[m]
+    if m != l - 1 and li[m + 1] > li[m]:
+        return find_peak(li[m + 1:])
+    return find_peak(li[:m])
