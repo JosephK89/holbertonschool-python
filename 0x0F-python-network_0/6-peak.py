b2b0996e649e2dab9ae24b9bbb41c923881dc85b
JosephK89 <josephkamel262@gmail.com>
2021-06-04 21:59:31 +0300

    Add project

--- 0x0F-python-network_0/6-peak.py
+++ 0x0F-python-network_0/6-peak.py
@@ -9,12 +9,13 @@ def find_peak(list_of_integers):
     if not list_of_integers:
         return None
     start = 0
+    l = len(list_of_integers)
     end = len(list_of_integers) - 1
     mid = (start + end) / 2
     mid = int(mid)
-    if end == 1:
+    if l == 1:
         return list_of_integers[0]
-    if end == 2:
+    if l == 2:
         return max(list_of_integers)
     if list_of_integers[mid] >= list_of_integers[mid - 1] and list_of_integers[mid] >= list_of_integers[mid + 1]:
         return list_of_integers[mid]
