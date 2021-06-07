1a3f523275836f9f5f07be545ed58f6ec2436c30
JosephK89 <josephkamel262@gmail.com>
2021-06-07 17:01:58 +0300

    Add python project

--- 0x10-python-network_1/6-post_email.py
+++ 0x10-python-network_1/6-post_email.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-takes in a url and an email, sends a post request to the passed url with the
-email as a parameter, and displays the body of the response
+takes in a url and an email address, sends a post request to the passed url with the email as a parameter, and finally displays the body of the response
 """
 import requests
 import sys
 
 if __name__ == "__main__":
-    r = request.post(sys.argv[1], data={"email": sys.argv[2]})
+
+    r = requests.post(sys.argv[1], data={"email": sys.argv[2]})
     print(r.text)
