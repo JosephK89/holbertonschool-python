74e92724d955bab32d910ccfaffb963048554b40
JosephK89 <josephkamel262@gmail.com>
2021-06-07 17:19:02 +0300

    Add python project

--- 0x10-python-network_1/3-error_code.py
+++ 0x10-python-network_1/3-error_code.py
@@ -5,7 +5,7 @@ takes in a url, send a request to the url and displays the body of the response
 import urllib.request
 import sys
 
-if __name__=="__main__":
+if __name__ == "__main__":
     try:
         with urllib.request.urlopen(sys.argv[1]) as res:
             print(res.read().decode("utf-8"))
