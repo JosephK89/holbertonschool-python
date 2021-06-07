56666221b6a02fc655f3464855aee7912216c9eb
JosephK89 <josephkamel262@gmail.com>
2021-06-07 16:40:44 +0300

    Add python project

--- 0x10-python-network_1/3-error_code.py
+++ 0x10-python-network_1/3-error_code.py
@@ -3,12 +3,11 @@
 takes in a url, sends a request to the url and displays the body of the response
 """
 import urllib.request
-from urllib.error import HTTPError
 import sys
 
 if __name__ == "__main__":
     try:
         with urllib.request.urlopen(sys.argv[1]) as res:
             print(res.read().decode("utf-8"))
-    except HTTPError as ex:
-        print("Error code: {}".format(ex.code))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
