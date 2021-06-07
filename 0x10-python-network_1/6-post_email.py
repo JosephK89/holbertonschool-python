29864402dd546eee1f4fc0e8a0d402760599b960
JosephK89 <josephkamel262@gmail.com>
2021-06-07 18:13:31 +0300

    Add python project

--- 0x10-python-network_1/6-post_email.py
+++ 0x10-python-network_1/6-post_email.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-takes in a url and an email address, sends a post request to the passed url with the email as a parameter, and finally display the body of the response
+takes in a url and an email address, sends a post request to the passed url
+with the email as a parameter, and finally displays the body of the response
 """
-import requests
-from sys import argv
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    import requests
+    from sys import argv
     r = requests.post(argv[1], data={"email": argv[2]})
     print(r.text)
--- 0x10-python-network_1/7-error_code.py
+++ 0x10-python-network_1/7-error_code.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """
-takes in a url, sends a request to the url and displays the body of the response
+takes in a url, sends a request to the URL and displays the body of the response
 """
-import requests
-import sys
-
-if __name__ == "__main__":
-    r = request.get(sys.argv[1])
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    r = requests.get(argv[1])
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
