666e2bb418c0b5b527a9c0a5654cdc876e4e9c2f
JosephK89 <josephkamel262@gmail.com>
2021-06-07 18:20:34 +0300

    Add python project

--- 0x10-python-network_1/7-error_code.py
+++ 0x10-python-network_1/7-error_code.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
-takes in a url, sends a request to the URL and displays the body of the response
+takes in a url, sends a request to the url and displays the body of the response
 """
 if __name__ == '__main__':
     import requests
     from sys import argv
     r = requests.get(argv[1])
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+    status = r.status_code
+    if status >= 400:
+        print("Error code: " + str(status))
     else:
         print(r.text)
