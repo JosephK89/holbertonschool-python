59f90d4a87be5ad970d9f99a3f6040fc56bd5bdf
JosephK89 <josephkamel262@gmail.com>
2021-06-08 13:10:39 +0300

    Add python project

--- 0x11-utf8_validation/0-validate_utf8.py
+++ 0x11-utf8_validation/0-validate_utf8.py
@@ -8,10 +8,10 @@ def validUTF8(data):
     """validUTF8 function"""
     bytes = 0;
     for num in data:
-        bin_rep = format(num, "#010b")[-8:]
+        bin_rep = format(num, '#010b')[-8:]
         if byte == 0:
             for bit in bin_rep:
-                if bit == "0":
+                if bit == '0':
                     break
                 bytes += 1
             if bytes == 0:
@@ -19,7 +19,7 @@ def validUTF8(data):
             if bytes == 1 or bytes > 4:
                 return False
         else:
-            if not (bin_rep[0] == "1" and bin_rep[1] == "0"):
+            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
                 return False
         bytes -= 1
     return bytes == 0
