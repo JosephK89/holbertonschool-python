ee8c5643295e8448405d0a722a8714575f307f68
JosephK89 <josephkamel262@gmail.com>
2021-06-16 10:31:47 +0000

    ADD sql project

---
+++ 0x15-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+--Write a SQL script that creates a table users
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email CHAR(255) NOT NULL UNIQUE,
+    name CHAR(255),
+    country ENUM ('US', 'CO', 'TN') NOT NULL
+);
