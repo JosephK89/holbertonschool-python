cefe38d1d10d3a42e46e03d45e698d146c13e111
JosephK89 <75435909+JosephK89@users.noreply.github.com>
2021-06-18 23:08:10 +0300

    Update 0-count.py

--- 0x16-count_it/0-count.py
+++ 0x16-count_it/0-count.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Write a recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords
+module
 """
 from collections import Counter, defaultdict
 import re
