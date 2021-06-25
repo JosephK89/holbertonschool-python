7eaef3728cfa08c3ee9e5ef941ab0dfecf3b6e73
JosephK89 <josephkamel262@gmail.com>
2021-06-25 22:43:35 +0000

    Fix

--- 0x15-MySQL_Advanced/0-uniq_users.sql
+++ 0x15-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,7 @@
 --Write a SQL script that creates a table users
-CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	email CHAR(255) UNIQUE NOT NULL,
-	name CHAR(255));
+CREATE TABLE IF NOT EXISTS users(
+       id INT UNIQUE NOT NULL AUTO_INCREMENT,
+       EMAIL CHAR(255) UNIQUE NOT NULL,
+       name CHAR(255),
+       PRIMARY KEY (ID)
+);
--- 0x15-MySQL_Advanced/1-country_users.sql
+++ 0x15-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,8 @@
 --Write a SQL script that creates a table users
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    email CHAR(255) NOT NULL UNIQUE,
-    name CHAR(255),
-    country ENUM ('US', 'CO', 'TN') NOT NULL
+CREATE TABLE IF NOT EXISTS users(
+       id INT UNIQUE NOT NULL AUTO_INCREMENT,
+       EMAIL CHAR(255) UNIQUE NOT NULL,
+       name CHAR(255),
+       country ENUM("US","CO","TN") NOT NULL,
+       PRIMARY KEY (ID)
 );
